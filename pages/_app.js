ceffb5fa8fa1daa3043e9692eaa90fb6614cc013
Charlie <bandstracharlie@gmail.com>
2023-04-10 03:59:22 -0600

    convert to es6

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
+import React from 'react'
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
+
+export default MyApp
